8a49b949f36f4a7233cae3bdd5aefe3db246023e
chris <chris438@gmail.com>
2016-11-19 15:59:05 -0800

    Add blueprint for proposer

--- Proposer.java
+++ Proposer.java
@@ -3,14 +3,46 @@ public class Proposer{
 	
 	private Messenger messenger;
 	private Integer currentProposerId;
+	//from all the different acceptors 
 	private Set<Packet> promises;
 	private Packet paxosInfoPacket;
+	
+	private Integer lastValueReceived;
+	private Integer valueForOperation;
 	public Proposer() {
 		promises = new HashSet<Packet>();
 		
 	}
 	
-	public void prepare() {
-        
+	public void prepare(Integer operation) {
+		//dont need any promises. starting fresh
+		promises.clear();
+		currentProposerId = currentProposerId + 1;
+        //send to every acceptor
+		messenger.sendProposal(currentProposalId);
+		valueForOperation = operation;
+	}
+	
+	public void receivePromise(Packet promise) {
+	    String acceptorIpAddress = promise.getFromIpAddress();
+	    Integer proposalId = promise.getProposalId();
+	    if(promise.getValue() != null) {
+	    	if(promise.getValue().equals(lastValueReceived)) {
+	    		//go ahead and commit this
+	    		messenger.sendCommitMessage(proposalId, lastValueReceived);
+	    	} else {
+	    	    //another operation is in flight
+	    	    Thread.sleep(1000);
+	    	    lastValueReceived = promise.getValue();
+			    messenger.sendProposal(currentProposalId);
+	    	}
+	    }
+	    if(proposalId.equals(currentProposerId) && promises.contains(promise)) {
+	    	promises.add(promise);
+	    	if(promises.size().equals(QUORUM_SIZE)) {
+	    		messenger.sendAcceptMessage(proposalId, valueForOperation);
+	    	}
+	    }
+	   
 	}
 }
